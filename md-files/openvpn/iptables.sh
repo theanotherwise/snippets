58023e4ec29625104fa8dea5592c9ba9df71dc92
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2022-08-11 01:36:49 +0200

    Update iptables.sh

--- md-files/openvpn/iptables.sh
+++ md-files/openvpn/iptables.sh
@@ -1,25 +1,35 @@
+export HOST_IP="10.100.255.2"
+export HOST_NET="10.100.255.0/24"
+export VPN_NET="10.8.0.0/24"
+
 iptables-restore << EndOfMessage
 *filter
 :INPUT ACCEPT [0:0]
 :FORWARD DROP [0:0]
 :OUTPUT ACCEPT [0:0]
 :vpn-aaa - [0:0]
--A INPUT -s 10.8.0.0/24 -j vpn-aaa
--A INPUT -s 10.8.0.0/24 -d 10.100.255.0/24 -j DROP
--A INPUT -p tcp -m tcp --dport 1194 -j ACCEPT
-
--A vpn-aaa -s 10.8.0.0/24 -p tcp --dport 22 -d 10.100.255.2 -j ACCEPT
--A vpn-aaa -s 10.8.0.0/24 -p tcp --dport 80 -d 10.100.255.2 -j ACCEPT
--A vpn-aaa -j DROP
+-A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+-A INPUT -p tcp -s ${VPN_NET} -j vpn-aaa
+-A INPUT -p tcp --dport 22      -j ACCEPT
+-A INPUT -p udp --dport 53      -j ACCEPT
+-A INPUT -p tcp --dport 1194    -j ACCEPT
+-A INPUT -j DROP
 
 -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
--A FORWARD -s 10.8.0.0/24 -j ACCEPT
+-A FORWARD -i tun0 -p tcp -s ${VPN_NET} -j ACCEPT
+-A FORWARD -j DROP
+
+# -A vpn-aaa -i tun0 -p tcp -s ${VPN_NET} -d ${HOST_IP} --dport 22    -j ACCEPT
+# -A vpn-aaa -i tun0 -p tcp -s ${VPN_NET} -d ${HOST_IP} --dport 80    -j ACCEPT
+# -A vpn-aaa -i tun0 -p tcp -s ${VPN_NET} -d ${HOST_IP} --dport 443   -j ACCEPT
+-A vpn-aaa -i tun0 -s ${VPN_NET} -d ${HOST_IP} -j DROP
+-A vpn-aaa -j DROP
 COMMIT
 *nat
 :PREROUTING ACCEPT [0:0]
 :INPUT ACCEPT [0:0]
 :OUTPUT ACCEPT [0:0]
 :POSTROUTING ACCEPT [0:0]
--A POSTROUTING -s 10.8.0.0/24 ! -d 10.8.0.0/24 -j SNAT --to-source 10.100.255.2
+-A POSTROUTING -s ${VPN_NET} ! -d ${VPN_NET} -j SNAT --to-source ${HOST_IP}
 COMMIT
 EndOfMessage
