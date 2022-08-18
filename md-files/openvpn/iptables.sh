e8bc033a31a4a18f3b95b19cfaf7f8fd9dc947cf
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2022-08-19 00:34:03 +0200

    Update iptables.sh

--- md-files/openvpn/iptables.sh
+++ md-files/openvpn/iptables.sh
@@ -1,35 +1,49 @@
 export HOST_IP="10.100.255.2"
 export HOST_NET="10.100.255.0/24"
-export VPN_NET="10.8.0.0/24"
+export VPN_AAA_NET="10.8.0.0/24"
 
 iptables-restore << EndOfMessage
 *filter
 :INPUT ACCEPT [0:0]
 :FORWARD DROP [0:0]
 :OUTPUT ACCEPT [0:0]
-:vpn-aaa - [0:0]
+:vpn-aaa-input - [0:0]
+:vpn-aaa-forward - [0:0]
+:drop-private-networks - [0:0]
+
+# INPUT
 -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
--A INPUT -p tcp -s ${VPN_NET} -j vpn-aaa 
--A INPUT -p tcp --dport 22      -j ACCEPT
--A INPUT -p udp --dport 53      -j ACCEPT
--A INPUT -p tcp --dport 1194    -j ACCEPT
+-A INPUT -s ${VPN_AAA_NET} -j vpn-aaa-input
+-A INPUT -p tcp --dport 22 -j ACCEPT
+-A INPUT -p udp --dport 53 -j ACCEPT
+-A INPUT -p tcp --dport 1194 -j ACCEPT
 -A INPUT -j DROP
 
+# INPUT (VPN AAA -> Host)
+-A vpn-aaa-input -d ${HOST_IP} -p tcp --dport 22 -j ACCEPT
+-A vpn-aaa-input -j DROP
+
+# FORWARD
 -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
--A FORWARD -i tun0 -p tcp -s ${VPN_NET} -j ACCEPT
+-A FORWARD -s ${VPN_AAA_NET} -j vpn-aaa-forward
 -A FORWARD -j DROP
 
-# -A vpn-aaa -i tun0 -p tcp -s ${VPN_NET} -d ${HOST_IP} --dport 22    -j ACCEPT
-# -A vpn-aaa -i tun0 -p tcp -s ${VPN_NET} -d ${HOST_IP} --dport 80    -j ACCEPT
-# -A vpn-aaa -i tun0 -p tcp -s ${VPN_NET} -d ${HOST_IP} --dport 443   -j ACCEPT
--A vpn-aaa -i tun0 -s ${VPN_NET} -d ${HOST_IP} -j DROP
--A vpn-aaa -j DROP
+# FORWARD (VPN AAA -> Private Access)
+-A vpn-aaa-forward -d 10.100.10.2/32 -p tcp --dport 22 -j ACCEPT
+-A vpn-aaa-forward -j drop-private-networks
+
+# FORWARD (Drop Private Networks)
+-A drop-private-networks -d 10.0.0.0/8 -j DROP
+-A drop-private-networks -d 172.16.0.0/12 -j DROP
+-A drop-private-networks -d 192.168.0.0/16 -j DROP
+-A drop-private-networks -j ACCEPT
+
 COMMIT
 *nat
 :PREROUTING ACCEPT [0:0]
 :INPUT ACCEPT [0:0]
 :OUTPUT ACCEPT [0:0]
 :POSTROUTING ACCEPT [0:0]
--A POSTROUTING -s ${VPN_NET} ! -d ${VPN_NET} -j SNAT --to-source ${HOST_IP}
+-A POSTROUTING -s ${VPN_AAA_NET} ! -d ${VPN_AAA_NET} -j SNAT --to-source ${HOST_IP}
 COMMIT
 EndOfMessage
