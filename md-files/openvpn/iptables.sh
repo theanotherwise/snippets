144a02f365b115b56d1ee5466802b48a540a5efd
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2022-09-12 01:33:21 +0200

    Update iptables.sh

--- md-files/openvpn/iptables.sh
+++ md-files/openvpn/iptables.sh
@@ -2,7 +2,6 @@ export HOST_IP="10.100.255.2"
 export HOST_NET="10.100.255.0/24"
 
 export VPN_AAA_NET="10.8.0.0/24"
-export VPN_BBB_NET="10.9.0.0/24"
 
 export A_CLASS_NETWORK="10.0.0.0/8"
 export B_CLASS_NETWORK="172.16.0.0/12"
@@ -15,14 +14,11 @@ iptables-restore << EndOfMessage
 :OUTPUT ACCEPT [0:0]
 :vpn-aaa-input - [0:0]
 :vpn-aaa-forward - [0:0]
-:vpn-bbb-input - [0:0]
-:vpn-bbb-forward - [0:0]
 :drop-private-networks - [0:0]
 
 # INPUT
 -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 -A INPUT -s ${VPN_AAA_NET} -j vpn-aaa-input
--A INPUT -s ${VPN_BBB_NET} -j vpn-bbb-input
 -A INPUT -p tcp --dport 22 -j ACCEPT
 -A INPUT -p udp --dport 53 -j ACCEPT
 -A INPUT -p tcp --dport 1194 -j ACCEPT
@@ -32,23 +28,14 @@ iptables-restore << EndOfMessage
 -A vpn-aaa-input -d ${HOST_IP} -p tcp --dport 22 -j ACCEPT
 -A vpn-aaa-input -j DROP
 
-# INPUT (VPN BBB -> Host)
--A vpn-bbb-input -d ${HOST_IP} -p tcp --dport 22 -j ACCEPT
--A vpn-bbb-input -j DROP
-
 # FORWARD
 -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
 -A FORWARD -s ${VPN_AAA_NET} -j vpn-aaa-forward
--A FORWARD -s ${VPN_BBB_NET} -j vpn-aaa-forward
 -A FORWARD -j DROP
 
 # FORWARD (VPN AAA -> Private Access)
 -A vpn-aaa-forward -d 10.100.10.2/32 -p tcp --dport 22 -j ACCEPT
 -A vpn-aaa-forward -j drop-private-networks
-
-# FORWARD (VPN BBB -> Private Access)
--A vpn-bbb-forward -d 10.100.10.2/32 -p tcp --dport 22 -j ACCEPT
--A vpn-bbb-forward -j drop-private-networks
 
 # FORWARD (Drop Private Networks)
 -A drop-private-networks -d ${A_CLASS_NETWORK} -j DROP
@@ -63,6 +50,5 @@ COMMIT
 :OUTPUT ACCEPT [0:0]
 :POSTROUTING ACCEPT [0:0]
 -A POSTROUTING -s ${VPN_AAA_NET} ! -d ${VPN_AAA_NET} -j SNAT --to-source ${HOST_IP}
--A POSTROUTING -s ${VPN_BBB_NET} ! -d ${VPN_BBB_NET} -j SNAT --to-source ${HOST_IP}
 COMMIT
 EndOfMessage
